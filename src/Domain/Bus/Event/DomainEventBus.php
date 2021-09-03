1471b8cd838393a1b3c35f2ec7f5f236b6213adf
pascualmg <pascual.munoz@pccomponentes.com>
2021-09-03 22:51:11 +0200

    @

--- src/Domain/Bus/Event/DomainEventBus.php
+++ src/Domain/Bus/Event/DomainEventBus.php
@@ -6,5 +6,6 @@ namespace pascualmg\dddfinitions\Domain\Bus\Event;
 interface DomainEventBus
 {
     public function dispatch(DomainEvent ...$domainEvents): void;
+    public function subscribe(DomainEventSubscriber ...$domainEventSubscribers);
 }
 
